5859afb877b6c42c0578f900f32859961ada7bde
Collin McNeese <collinmcneese@github.com>
2024-05-11 14:36:32 -0500

    Update registration token handling in start.sh

--- runner-no-dind/start.sh
+++ runner-no-dind/start.sh
@@ -11,14 +11,22 @@ if [[ -n ${GHRUNNER_GITHUB_BASE_URL} ]]; then
     else
       REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GHRUNNER_ACCESS_TOKEN}" ${GHRUNNER_GITHUB_BASE_URL}/api/v3/repos/${GHRUNNER_ORGANIZATION}/${GHRUNNER_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
     fi
-    ./config.sh --url "${GHRUNNER_GITHUB_BASE_URL}/${GHRUNNER_ORGANIZATION}/${GHRUNNER_REPOSITORY}" --token "${REG_TOKEN}" --unattended --labels "${GHRUNNER_LABELS}" --ephemeral
+    ./config.sh --url "${GHRUNNER_GITHUB_BASE_URL}/${GHRUNNER_ORGANIZATION}/${GHRUNNER_REPOSITORY}" \
+      --token "${REG_TOKEN}" \
+      --unattended \
+      --labels "${GHRUNNER_LABELS}" \
+      --ephemeral ${GHRUNNER_RUNNER_GROUP:+--runnergroup "${GHRUNNER_RUNNER_GROUP}"}
   else
     if [[ -n ${GHRUNNER_REG_TOKEN} ]]; then
       REG_TOKEN=${GHRUNNER_REG_TOKEN}
     else
       REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GHRUNNER_ACCESS_TOKEN}" ${GHRUNNER_GITHUB_BASE_URL}/api/v3/orgs/${GHRUNNER_ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
     fi
-    ./config.sh --url "${GHRUNNER_GITHUB_BASE_URL}/${GHRUNNER_ORGANIZATION}" --token "${REG_TOKEN}" --unattended --labels "${GHRUNNER_LABELS}" --ephemeral
+    ./config.sh --url "${GHRUNNER_GITHUB_BASE_URL}/${GHRUNNER_ORGANIZATION}" \
+      --token "${REG_TOKEN}" \
+      --unattended \
+      --labels "${GHRUNNER_LABELS}" \
+      --ephemeral ${GHRUNNER_RUNNER_GROUP:+--runnergroup "${GHRUNNER_RUNNER_GROUP}"}
   fi
 else # Register with the GitHub endpoint
   if [[ -n ${GHRUNNER_REPOSITORY} ]]; then
@@ -27,14 +35,22 @@ else # Register with the GitHub endpoint
     else
       REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GHRUNNER_ACCESS_TOKEN}" https://api.github.com/repos/${GHRUNNER_ORGANIZATION}/${GHRUNNER_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
     fi
-    ./config.sh --url "https://github.com/${GHRUNNER_ORGANIZATION}/${GHRUNNER_REPOSITORY}" --token "${REG_TOKEN}" --unattended --labels "${GHRUNNER_LABELS}" --ephemeral
+    ./config.sh --url "https://github.com/${GHRUNNER_ORGANIZATION}/${GHRUNNER_REPOSITORY}" \
+      --token "${REG_TOKEN}" \
+      --unattended \
+      --labels "${GHRUNNER_LABELS}" \
+      --ephemeral ${GHRUNNER_RUNNER_GROUP:+--runnergroup "${GHRUNNER_RUNNER_GROUP}"}
   else
     if [[ -n ${GHRUNNER_REG_TOKEN} ]]; then
       REG_TOKEN=${GHRUNNER_REG_TOKEN}
     else
       REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GHRUNNER_ACCESS_TOKEN}" https://api.github.com/orgs/${GHRUNNER_ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
     fi
-    ./config.sh --url "https://github.com/${GHRUNNER_ORGANIZATION}" --token "${REG_TOKEN}" --unattended --labels "${GHRUNNER_LABELS}" --ephemeral
+    ./config.sh --url "https://github.com/${GHRUNNER_ORGANIZATION}" \
+      --token "${REG_TOKEN}" \
+      --unattended \
+      --labels "${GHRUNNER_LABELS}" \
+      --ephemeral ${GHRUNNER_RUNNER_GROUP:+--runnergroup "${GHRUNNER_RUNNER_GROUP}"}
   fi
 fi
 
